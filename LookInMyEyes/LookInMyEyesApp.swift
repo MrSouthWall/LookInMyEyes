04822eb15be2a43119b1b209cd35678fb79f69e3
MrSouthWall <147359717+MrSouthWall@users.noreply.github.com>
2025-07-19 18:59:46 +0800

    Initial Commit

---
+++ LookInMyEyes/LookInMyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookInMyEyesApp.swift
+//  LookInMyEyes
+//
+//  Created by MrSouthWall on 2025/7/19.
+//
+
+import SwiftUI
+
+@main
+struct LookInMyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
